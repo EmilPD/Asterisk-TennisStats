5a06231d5cdc072d8435f612bf4aa9ef7a9aa658
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-06-04 23:01:04 +0300

    Added Coach Configuration File

---
+++ ATPTennisStat/ATPTennisStat.SQLServerData/EntityConfigurations/CoachConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using ATPTennisStat.Models;
+
+namespace ATPTennisStat.SQLServerData
+{
+    internal class CoachConfiguration : EntityTypeConfiguration<Coach>
+    {
+        public CoachConfiguration()
+        {
+            this.HasKey(c => c.Id);
+
+            this.Property(c => c.FirstName)
+                .IsRequired()
+                .HasMaxLength(40);
+
+            this.Property(c => c.LastName)
+                .IsRequired()
+                .HasMaxLength(40);
+
+            this.Property(c => c.BirthDate)
+                .HasColumnType("smalldatetime");
+        }
+    }
+}
