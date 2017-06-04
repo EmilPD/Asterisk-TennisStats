498a218a60ba56dac4ed68cbbd716916e024729a
Emil Dimitrov <emil_132@abv.bg>
2017-06-04 23:11:02 +0300

    Added Player Configuration

---
+++ ATPTennisStat/ATPTennisStat.SQLServerData/EntityConfigurations/PlayerConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using ATPTennisStat.Models;
+
+namespace ATPTennisStat.SQLServerData.EntityConfigurations
+{
+    public class PlayerConfiguration : EntityTypeConfiguration<Player>
+    {
+        public PlayerConfiguration()
+        {
+            Property(p => p.FirstName)
+                .IsRequired()
+                .HasMaxLength(50)
+                .HasColumnAnnotation(
+                    IndexAnnotation.AnnotationName,
+                    new IndexAnnotation(
+                        new IndexAttribute("IX_FirstName", 1)
+                        {
+                            IsUnique = true,
+                            IsClustered = false
+                        }));
+
+            Property(p => p.LastName)
+                .IsRequired()
+                .HasMaxLength(50)
+                .HasColumnAnnotation(
+                    IndexAnnotation.AnnotationName,
+                    new IndexAnnotation(
+                        new IndexAttribute("IX_LastName", 1)
+                        {
+                            IsUnique = true,
+                            IsClustered = false
+                        }));
+
+            Property(p => p.BirthDate)
+                .HasColumnType("smalldatetime")
+                .IsOptional();
+        }
+    }
+}
