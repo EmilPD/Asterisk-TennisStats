76ace1ab432aff04ddcd62775014b65949e1acc2
Emil Dimitrov <emil_132@abv.bg>
2017-06-04 23:36:15 +0300

    Testing in ConsoleClient

--- ATPTennisStat/ATPTennisStat.ConsoleClient/Startup.cs
+++ ATPTennisStat/ATPTennisStat.ConsoleClient/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using ATPTennisStat.SQLServerData;
+using ATPTennisStat.Models;
 
 namespace ATPTennisStat.ConsoleClient
 {
@@ -9,17 +10,17 @@ namespace ATPTennisStat.ConsoleClient
         static void Main()
         {
             var context = new SqlServerDbContext();
-            
-            //context.Cities.Add(new City
-            //{
-            //    Name = "Varna"
-            //    //Country = new Country { Name = "Bulgaria" },
-            //    //Players = new List<Player> { new Player { FirstName = "Ivan" } },
-            //    //Tournaments = new List<Tournament> { new Tournament
-            //    //    { Name = "Paris", PrizeMoney = 10 } }
-            //});
 
-            //context.SaveChanges();
+            context.Cities.Add(new City
+            {
+                Name = "Varna",
+                Country = new Country { Name = "Bulgaria" }
+                //Players = new List<Player> { new Player { FirstName = "Ivan" } },
+                //Tournaments = new List<Tournament> { new Tournament
+                //    { Name = "Paris", PrizeMoney = 10 } }
+            });
+
+            context.SaveChanges();
 
             var selectedCity = context.Cities
                 .Where(c => c.Name == "Varna")
