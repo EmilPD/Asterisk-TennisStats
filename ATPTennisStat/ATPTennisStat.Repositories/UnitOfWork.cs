42e45fb77bb83a19d32bae6b3afdcbc1c2461666
Emil Dimitrov <emil_132@abv.bg>
2017-06-04 12:03:02 +0300

    Added generic repository interfaces

---
+++ ATPTennisStat/ATPTennisStat.Repositories/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using ATPTennisStat.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATPTennisStat.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ATPTennisStatContext context;
+
+        public UnitOfWork(ATPTennisStatContext context)
+        {
+            this.context = context;
+        }
+
+        public int Finished()
+        {
+            return context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
--- ATPTennisStat/ATPTennisStat.WPFClient/App.xaml.cs
+++ ATPTennisStat/ATPTennisStat.WPFClient/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace ATPTennisStat
+namespace ATPTennisStat.WPFClient
 {
     /// <summary>
     /// Interaction logic for App.xaml
