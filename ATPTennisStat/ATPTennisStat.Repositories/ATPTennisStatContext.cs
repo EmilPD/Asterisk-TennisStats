7c2e43f27209ef82eb88c1c1da3abac5cf9481d7
Emil Dimitrov <emil_132@abv.bg>
2017-06-04 12:42:46 +0300

    small change

---
+++ ATPTennisStat/ATPTennisStat.Repositories/ATPTennisStatContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace ATPTennisStat.Repositories
+{
+    public class ATPTennisStatContext : DbContext
+    {
+        public ATPTennisStatContext()
+            : base("ATPTennisStatContext")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
