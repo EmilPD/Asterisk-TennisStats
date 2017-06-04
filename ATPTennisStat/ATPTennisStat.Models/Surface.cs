664f9a66d1c4de0ad63f94018df95e85ab1e104a
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-06-04 16:11:38 +0300

    Added Surface.cs class

---
+++ ATPTennisStat/ATPTennisStat.Models/Surface.cs
@@ -0,0 +1,11 @@
+﻿namespace ATPTennisStat.Models
+{
+    public class Surface
+    {
+        public int Id { get; set; }
+
+        public string Type { get; set; }
+
+        public string Speed { get; set; }
+    }
+}
