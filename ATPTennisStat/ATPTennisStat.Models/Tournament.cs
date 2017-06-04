9426db619a4d91e04094d38d4efbdd8517cc8faa
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-06-04 16:09:30 +0300

    Added Tournaments Class

---
+++ ATPTennisStat/ATPTennisStat.Models/Tournament.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATPTennisStat.Models
+{
+    public class Tournament
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public decimal PrizeMoney { get; set; }
+
+        public virtual Surface Type { get; set; }
+
+        public virtual City City { get; set; } // One City - many Tournaments
+    }
+}
