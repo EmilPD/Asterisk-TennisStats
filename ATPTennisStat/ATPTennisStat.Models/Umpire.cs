c1eb741e91942b58d3ce23302606eece166c1830
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-06-04 16:25:39 +0300

    Added Umpire.cs class to Models Made changes to Tournaments - removed unnecessary usings

---
+++ ATPTennisStat/ATPTennisStat.Models/Umpire.cs
@@ -0,0 +1,15 @@
+﻿namespace ATPTennisStat.Models
+{
+    class Umpire
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public ushort YearActiveFrom { get; set; }
+
+        public virtual Country Country { get; set; }
+    }
+}
