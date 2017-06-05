2ccdc117afc432c53394effa1a29690dae05d186
Emil Dimitrov <emil_132@abv.bg>
2017-06-05 18:18:14 +0300

    Testing Repository in Console Client

--- ATPTennisStat/ATPTennisStat.ConsoleClient/Startup.cs
+++ ATPTennisStat/ATPTennisStat.ConsoleClient/Startup.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using ATPTennisStat.SQLServerData;
 using ATPTennisStat.Models;
+using ATPTennisStat.Repositories;
 
 namespace ATPTennisStat.ConsoleClient
 {
@@ -9,18 +10,19 @@ namespace ATPTennisStat.ConsoleClient
     {
         static void Main()
         {
+            //var unitOfWork = new SqlServerUnitOfWork(new SqlServerDbContext());
+
             var context = new SqlServerDbContext();
+            //context.Cities.Add(new City
+            //{
+            //    Name = "Varna",
+            //    Country = new Country { Name = "Bulgaria" }
+            //    //Players = new List<Player> { new Player { FirstName = "Ivan" } },
+            //    //Tournaments = new List<Tournament> { new Tournament
+            //    //    { Name = "Paris", PrizeMoney = 10 } }
+            //});
 
-            context.Cities.Add(new City
-            {
-                Name = "Varna",
-                Country = new Country { Name = "Bulgaria" }
-                //Players = new List<Player> { new Player { FirstName = "Ivan" } },
-                //Tournaments = new List<Tournament> { new Tournament
-                //    { Name = "Paris", PrizeMoney = 10 } }
-            });
-
-            context.SaveChanges();
+            //context.SaveChanges();
 
             var selectedCity = context.Cities
                 .Where(c => c.Name == "Varna")
@@ -30,6 +32,12 @@ namespace ATPTennisStat.ConsoleClient
                 })
                 .FirstOrDefault();
             Console.WriteLine(selectedCity);
+
+            //var citiesCollection = unitOfWork.Cities.GetAll();
+            //foreach (var city in citiesCollection)
+            //{
+            //    Console.WriteLine(city.Name);
+            //}
         }
     }
 }
