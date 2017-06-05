ffb042d4f57b63e0443181c5a4d2e174508d6d5a
Emil Dimitrov <emil_132@abv.bg>
2017-06-05 23:27:52 +0300

    Deleted Migrations

--- ATPTennisStat/ATPTennisStat.ConsoleClient/Startup.cs
+++ ATPTennisStat/ATPTennisStat.ConsoleClient/Startup.cs
@@ -6,7 +6,6 @@ using ATPTennisStat.Repositories;
 using System.Data.Entity;
 using ATPTennisStat.SQLServerData.Migrations;
 
-
 namespace ATPTennisStat.ConsoleClient
 {
     class Startup
@@ -26,14 +25,14 @@ namespace ATPTennisStat.ConsoleClient
         static void DbContextStart()
         {
             var context = new SqlServerDbContext();
-            //context.Cities.Add(new City
-            //{
-            //    Name = "Varna",
-            //    Country = new Country { Name = "Bulgaria" }
-            //    //Players = new List<Player> { new Player { FirstName = "Ivan" } },
-            //    //Tournaments = new List<Tournament> { new Tournament
-            //    //    { Name = "Paris", PrizeMoney = 10 } }
-            //});
+            context.Cities.Add(new City
+            {
+                Name = "Varna",
+                Country = new Country { Name = "Bulgaria" }
+                //Players = new List<Player> { new Player { FirstName = "Ivan" } },
+                //Tournaments = new List<Tournament> { new Tournament
+                //    { Name = "Paris", PrizeMoney = 10 } }
+            });
 
 
 
