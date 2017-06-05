47072e4d39900ce5a8620a4302da23c92c7667f6
tinmanjk <tinmanjk@mail.com>
2017-06-05 21:03:20 +0300

    creating composite primary key for PointDistribution

--- ATPTennisStat/ATPTennisStat.Models/PointDistribution.cs
+++ ATPTennisStat/ATPTennisStat.Models/PointDistribution.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +10,10 @@ namespace ATPTennisStat.Models
 {
     public class PointDistribution
     {
-        [Key]
-        public int Id { get; set; }
-
-        [Required]
+        [Key, Column(Order = 0)]
         public int TournamentCategoryId { get; set; }
 
-        [Required]
+        [Key, Column(Order = 1)]
         public int RoundId { get; set; }
 
         public virtual TournamentCategory TournamentCategory { get; set; }
