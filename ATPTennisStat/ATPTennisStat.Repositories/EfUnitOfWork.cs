1c5d9f2e00f3248489c9cc36d67511261e9273e7
Emil Dimitrov <emil_132@abv.bg>
2017-06-06 09:47:46 +0300

    Added EfUnitOfWork

---
+++ ATPTennisStat/ATPTennisStat.Repositories/EfUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using ATPTennisStat.Repositories.Contracts;
+
+namespace ATPTennisStat.Repositories
+{
+    public class EfUnitOfWork : IUnitOfWork
+    {
+        private DbContext context;
+
+        public EfUnitOfWork(DbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Finished()
+        {
+            this.context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
