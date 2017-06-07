7314c8ea92e58b35dab171da00b43c15ab2fd8ab
Emil Dimitrov <emil_132@abv.bg>
2017-06-07 10:36:22 +0300

    Added unique constrant to Country.Name

--- ATPTennisStat/ATPTennisStat.Models/Country.cs
+++ ATPTennisStat/ATPTennisStat.Models/Country.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ATPTennisStat.Models
 {
@@ -17,6 +18,7 @@ namespace ATPTennisStat.Models
 
         [Required]
         [StringLength(50)]
+        [Index("IX_Country_Name", IsUnique = true)]
         public string Name { get; set; }
 
         public virtual ICollection<City> Cities
