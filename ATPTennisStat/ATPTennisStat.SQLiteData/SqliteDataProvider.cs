476bc0daf266bea93c60ea98465338871120f59a
Emil Dimitrov <emil_132@abv.bg>
2017-06-09 13:06:23 +0300

    Added Sqlite Data Provider

---
+++ ATPTennisStat/ATPTennisStat.SQLiteData/SqliteDataProvider.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using ATPTennisStat.Models.SqliteModels;
+using ATPTennisStat.Repositories.Contracts;
+
+namespace ATPTennisStat.SQLiteData
+{
+    public class SqliteDataProvider
+    {
+        private IRepository<Log> logs;
+        private Func<IUnitOfWork> unitOfWork;
+
+        public SqliteDataProvider(Func<IUnitOfWork> uow, IRepository<Log> logs)
+        {
+            this.logs = logs;
+            this.unitOfWork = uow;
+        }
+
+        public IRepository<Log> Tickets
+        {
+            get
+            {
+                return this.logs;
+            }
+
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Logs");
+                }
+
+                this.logs = value;
+            }
+        }
+        
+        public Func<IUnitOfWork> UnitOfWork
+        {
+            get
+            {
+                return unitOfWork;
+            }
+
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Unit of work");
+                }
+
+                unitOfWork = value;
+            }
+        }
+    }
+}
