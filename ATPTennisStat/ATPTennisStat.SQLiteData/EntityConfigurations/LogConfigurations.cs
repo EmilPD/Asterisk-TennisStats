af202152578fb3c67a00afde3bc69ccc26a38404
Emil Dimitrov <emil_132@abv.bg>
2017-06-09 08:44:22 +0300

    Added Log Entity configurations

---
+++ ATPTennisStat/ATPTennisStat.SQLiteData/EntityConfigurations/LogConfigurations.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using ATPTennisStat.Models.SqliteModels;
+
+namespace ATPTennisStat.SQLiteData.EntityConfigurations
+{
+    public class LogConfigurations : EntityTypeConfiguration<Log>
+    {
+        public LogConfigurations()
+        {
+            Property(c => c.Message)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
