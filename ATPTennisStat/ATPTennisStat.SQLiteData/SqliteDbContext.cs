0947365bbdc6677a33fb2887f8e3d9c879ec7b90
Emil Dimitrov <emil_132@abv.bg>
2017-06-09 08:42:15 +0300

    Added SQLiteDbContext

---
+++ ATPTennisStat/ATPTennisStat.SQLiteData/SqliteDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using ATPTennisStat.SQLiteData.EntityConfigurations;
+using ATPTennisStat.SQLiteData.Migrations;
+using ATPTennisStat.Models.SqliteModels;
+
+namespace ATPTennisStat.SQLiteData
+{
+    public class SqliteDbContext : DbContext
+    {
+        public SqliteDbContext() : base("ATPTennisStatSQLite")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<SqliteDbContext, Configuration>(true));
+        }
+
+        public virtual IDbSet<Log> Logs { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new LogConfigurations());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
