45095ee5d6dc89ab99059340bd86274f94a567e1
Emil Dimitrov <emil_132@abv.bg>
2017-06-09 08:43:46 +0300

    Enabled Sqlite migrations + initial migration and config

---
+++ ATPTennisStat/ATPTennisStat.SQLiteData/Migrations/201706090535466_Initial.cs
@@ -0,0 +1,27 @@
+namespace ATPTennisStat.SQLiteData.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Logs",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Message = c.String(nullable: false, maxLength: 50),
+                        TimeStamp = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Logs");
+        }
+    }
+}
---
+++ ATPTennisStat/ATPTennisStat.SQLiteData/Migrations/Configuration.cs
@@ -0,0 +1,30 @@
+using System.Data.SQLite.EF6.Migrations;
+using System.Data.Entity.Migrations;
+
+namespace ATPTennisStat.SQLiteData.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<ATPTennisStat.SQLiteData.SqliteDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            SetSqlGenerator("System.Data.SQLite", new SQLiteMigrationSqlGenerator());
+        }
+
+        protected override void Seed(ATPTennisStat.SQLiteData.SqliteDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
