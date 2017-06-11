1e61dfb0062fc3e33c202e4360dab6043b5968e7
Emil Dimitrov <emil_132@abv.bg>
2017-06-11 23:45:34 +0300

    Refactored reports commands

--- ATPTennisStat/ATPTennisStat.ConsoleClient/Core/Providers/SqLiteLogger.cs
+++ ATPTennisStat/ATPTennisStat.ConsoleClient/Core/Providers/SqLiteLogger.cs
@@ -8,27 +8,19 @@ namespace ATPTennisStat.ConsoleClient.Core.Providers
     public class SqLiteLogger : ILogger
     {
         private readonly ISqliteDataProvider provider;
-        //private readonly IWriter writer; //can be removed
 
-        public SqLiteLogger(ISqliteDataProvider provider/*, IWriter writer*/)
+        public SqLiteLogger(ISqliteDataProvider provider)
         {
             if (provider == null)
             {
                 throw new ArgumentNullException("SqliteDataProvider");
             }
-
-            //if (writer == null)
-            //{
-            //    throw new ArgumentNullException("writer");
-            //}
-
+    
             this.provider = provider;
-            //this.writer = writer;
         }
 
         public void Log(string message)
         {
-            //this.writer.WriteLine($"{message}!"); //can be removed
             this.provider.Logs.Add(new Log { Message = message, TimeStamp = DateTime.Now });
             this.provider.UnitOfWork.Finished();
         }
