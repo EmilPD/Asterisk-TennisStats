5285a44fd57b82adeb6ddc5d588b6a7057775dbb
Emil Dimitrov <emil_132@abv.bg>
2017-06-11 07:37:25 +0300

    Implemented Sqlite logger

--- ATPTennisStat/ATPTennisStat.ConsoleClient/Core/Providers/SqLiteLogger.cs
+++ ATPTennisStat/ATPTennisStat.ConsoleClient/Core/Providers/SqLiteLogger.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 using ATPTennisStat.ConsoleClient.Core.Contracts;
+using ATPTennisStat.Models.SqliteModels;
+using ATPTennisStat.SQLiteData;
 
 namespace ATPTennisStat.ConsoleClient.Core.Providers
 {
     public class SqLiteLogger : ILogger
     {
-        public void Log(object msg)
+        private readonly SqliteDataProvider provider;
+        private readonly IWriter writer;
+
+        public SqLiteLogger(SqliteDataProvider provider, IWriter writer)
         {
-            // TODO: implement to sqLite
-            Console.WriteLine($"Logging {msg}!");
+            if (provider == null)
+            {
+                throw new ArgumentNullException("SqliteDataProvider");
+            }
+
+            if (writer == null)
+            {
+                throw new ArgumentNullException("writer");
+            }
+
+            this.provider = provider;
+            this.writer = writer;
+        }
+
+        public void Log(string message)
+        {
+            this.writer.WriteLine($"{message}!");
+            this.provider.Logs.Add(new Log { Message = message, TimeStamp = DateTime.Now });
+            this.provider.UnitOfWork.Finished();
         }
     }
 }
