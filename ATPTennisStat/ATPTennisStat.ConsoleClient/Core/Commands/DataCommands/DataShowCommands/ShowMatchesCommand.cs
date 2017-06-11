02593b83e64c741e6b22135a0456d21f90851dd4
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-06-11 23:20:18 +0300

    Refactored Show methods

--- ATPTennisStat/ATPTennisStat.ConsoleClient/Core/Commands/DataCommands/DataShowCommands/ShowMatchesCommand.cs
+++ ATPTennisStat/ATPTennisStat.ConsoleClient/Core/Commands/DataCommands/DataShowCommands/ShowMatchesCommand.cs
@@ -1,4 +1,5 @@
 ﻿using ATPTennisStat.ConsoleClient.Core.Contracts;
+using ATPTennisStat.Models;
 using ATPTennisStat.SQLServerData;
 using System;
 using System.Collections.Generic;
@@ -26,52 +27,47 @@ namespace ATPTennisStat.ConsoleClient.Core.Commands.DataCommands.DataShowCommand
             var result = new StringBuilder();
             var matches = dp.Matches.GetAll();
 
-            foreach (var match in matches)
+            if (parameters.Count > 0)
             {
-                string winnerName = $"{match.Winner.FirstName} {match.Winner.LastName}";
-                string loserName = $"{match.Loser.FirstName} {match.Loser.LastName}";
-                int i = 0;
+                int matchId = -1;
+                int.TryParse(parameters[0], out matchId);
 
-                if (parameters.Count > 0)
+                if (matchId > 0)
                 {
-                    int matchId = -1;
-                    int.TryParse(parameters[0], out matchId);
+                    Match match = dp.Matches.Get(matchId);
 
-                    if (matchId > 0)
+                    if (match != null)
                     {
-                        if (match.Id == matchId)
-                        {
-                            string fullName = $"{winnerName} : {loserName}";
-                            result.AppendLine(fullName);
-                            result.AppendLine(new string('-', fullName.Length + 4));
-                            result.AppendLine($"    Result: {match.Result}");
-                            result.AppendLine($"    Date: {match.DatePlayed.ToString("d", DateTimeFormatInfo.InvariantInfo)}");
-                            result.AppendLine($"    Tournament: {match.Tournament.Name}");
-                            result.AppendLine($"    Round: {match.Round.Stage}");
-                            result.AppendLine($"    Surface: {match.Tournament.Type.Type}, {match.Tournament.Type.Speed}");
-                            result.AppendLine($"    City: {match.Tournament.City.Name}, {match.Tournament.City.Country.Name}");
-                        }
-                        else
-                        {
-                            if (i < matches.Count())
-                            {
-                                continue;
-                            }
-                            else
-                            {
-                                throw new ArgumentException($"Sorry, no player with id {matchId} exists!");
-                            }
-                        }
+                        string winnerName = $"{match.Winner.FirstName} {match.Winner.LastName}";
+                        string loserName = $"{match.Loser.FirstName} {match.Loser.LastName}";
 
-                        i++;
+                        string fullName = $"{winnerName} : {loserName}";
+                        result.AppendLine(fullName);
+                        result.AppendLine(new string('-', fullName.Length + 4));
+                        result.AppendLine($"    Result: {match.Result}");
+                        result.AppendLine($"    Date: {match.DatePlayed.ToString("d", DateTimeFormatInfo.InvariantInfo)}");
+                        result.AppendLine($"    Tournament: {match.Tournament.Name}");
+                        result.AppendLine($"    Round: {match.Round.Stage}");
+                        result.AppendLine($"    Surface: {match.Tournament.Type.Type}, {match.Tournament.Type.Speed}");
+                        result.AppendLine($"    City: {match.Tournament.City.Name}, {match.Tournament.City.Country.Name}");
                     }
                     else
                     {
-                        throw new ArgumentException($"Sorry, {matchId} is not a valid number!");
+                        throw new ArgumentException($"Sorry, no player with id {matchId} exists!");
                     }
                 }
                 else
                 {
+                    throw new ArgumentException($"Sorry, {matchId} is not a valid number!");
+                }
+            }
+            else
+            {
+                foreach (var match in matches)
+                {
+                    string winnerName = $"{match.Winner.FirstName} {match.Winner.LastName}";
+                    string loserName = $"{match.Loser.FirstName} {match.Loser.LastName}";
+
                     result.AppendLine($"Id: {match.Id} | Date: {match.DatePlayed.ToString("d", DateTimeFormatInfo.InvariantInfo)} | {winnerName} : {loserName} | Result: {match.Result}");
                 }
             }
--- ATPTennisStat/ATPTennisStat.ConsoleClient/Core/Commands/DataCommands/DataShowCommands/ShowPlayersCommand.cs
+++ ATPTennisStat/ATPTennisStat.ConsoleClient/Core/Commands/DataCommands/DataShowCommands/ShowPlayersCommand.cs
@@ -25,100 +25,97 @@ namespace ATPTennisStat.ConsoleClient.Core.Commands.DataCommands.DataShowCommand
             var result = new StringBuilder();
             var players = dp.Players.GetAll();
 
-            foreach (Player player in players)
+            if (parameters.Count > 0)
             {
-                int i = 0;
-                var matchesCount = dp
-                    .Matches
-                    .Find((m => (m.Winner.Id == player.Id || m.Loser.Id == player.Id)))
-                    .ToList().Count;
-
-                if (parameters.Count > 0)
+                int playerId = -1;
+                int.TryParse(parameters[0], out playerId);
+                if (playerId > 0)
                 {
-                    int playerId = -1;
-                    int.TryParse(parameters[0], out playerId);
-                    if (playerId > 0)
+                    Player player = dp.Players.Get(playerId);
+
+                    if (player != null)
                     {
-                        if (player.Id == playerId)
+                        string fullName = $"* {player.FirstName} {player.LastName} *";
+                        result.AppendLine(fullName);
+                        result.AppendLine(new string('-', fullName.Length + 4));
+                        result.AppendLine($"    Id: {playerId}");
+
+                        var matchesCount = dp
+                                .Matches
+                                .Find((m => (m.Winner.Id == player.Id || m.Loser.Id == player.Id)))
+                                .ToList().Count;
+
+                        //nullable fields check
+                        if (player.BirthDate != null)
                         {
-                            string fullName = $"* {player.FirstName} {player.LastName} *";
-                            result.AppendLine(fullName);
-                            result.AppendLine(new string('-', fullName.Length + 4));
-                            result.AppendLine($"    Id: {playerId}");
+                            DateTime birthDay = DateTime.Now;
+                            DateTime.TryParse(player.BirthDate.ToString(), out birthDay);
+                            int playerAge = Convert.ToInt32(Math.Floor((DateTime.Now.Subtract(birthDay).TotalDays / 360)));
 
-                            //nullable fields check
-                            if(player.BirthDate != null)
+                            if (playerAge > 1)
                             {
-                                DateTime birthDay = DateTime.Now;
-                                DateTime.TryParse(player.BirthDate.ToString(), out birthDay);
-                                int playerAge = Convert.ToInt32(Math.Floor((DateTime.Now.Subtract(birthDay).TotalDays / 360)));
-
-                                if (playerAge > 1)
-                                {
-                                    result.AppendLine($"    Age: {playerAge}");
-                                }
-                            }
-
-                            if (player.Weight != null)
-                            {
-                                result.AppendLine($"    Weight: {player.Weight} kg");
-                            }
-
-                            if(player.Height != null)
-                            {
-                                result.AppendLine($"    Height: {player.Height} cm");
-
-                            }
-
-                            if (player.Ranking != null)
-                            {
-                                result.AppendLine($"    Ranking: {player.Ranking}");
-                            }
-
-                            if (player.City != null)
-                            {
-                                result.AppendLine($"    City: {player.City.Name}, {player.City.Country.Name}");
-                            }
-
-                            var matchesWon = dp.Matches
-                                .Find(m => m.Winner.Id == playerId)
-                                .ToList();
-
-                            var allPoints = 0;
-
-                            foreach (Match match in matchesWon)
-                            {
-                                allPoints += dp.PointDistributions
-                                    .Find(p => p.Round.Stage == match.Round.Stage &&
-                                    p.TournamentCategory.Category == match.Tournament.Category.Category)
-                                    .Select(p => p.Points)
-                                    .FirstOrDefault();
-                            }
-
-                            result.AppendLine($"    Points won: {allPoints}");
-                            result.AppendLine($"    Matches played: {matchesCount}");
-                        }
-                        else
-                        {
-                            if (i < players.Count())
-                            {
-                                continue;
-                            }
-                            else
-                            {
-                                throw new ArgumentException($"Sorry, no player with id {playerId} exists!");
+                                result.AppendLine($"    Age: {playerAge}");
                             }
                         }
 
-                        i++;
+                        if (player.Weight != null)
+                        {
+                            result.AppendLine($"    Weight: {player.Weight} kg");
+                        }
+
+                        if (player.Height != null)
+                        {
+                            result.AppendLine($"    Height: {player.Height} cm");
+
+                        }
+
+                        if (player.Ranking != null)
+                        {
+                            result.AppendLine($"    Ranking: {player.Ranking}");
+                        }
+
+                        if (player.City != null)
+                        {
+                            result.AppendLine($"    City: {player.City.Name}, {player.City.Country.Name}");
+                        }
+
+                        var matchesWon = dp.Matches
+                            .Find(m => m.Winner.Id == playerId)
+                            .ToList();
+
+                        var allPoints = 0;
+
+                        foreach (Match match in matchesWon)
+                        {
+                            allPoints += dp.PointDistributions
+                                .Find(p => p.Round.Stage == match.Round.Stage &&
+                                p.TournamentCategory.Category == match.Tournament.Category.Category)
+                                .Select(p => p.Points)
+                                .FirstOrDefault();
+                        }
+
+                        result.AppendLine($"    Points won: {allPoints}");
+                        result.AppendLine($"    Matches played: {matchesCount}");
                     }
                     else
                     {
-                        throw new ArgumentException($"Sorry, {playerId} is not a valid number!");
+                        throw new ArgumentException($"Sorry, no player with id {playerId} exists!");
                     }
                 }
                 else
                 {
+                    throw new ArgumentException($"Sorry, {playerId} is not a valid number!");
+                }
+            }
+            else
+            {
+                foreach (Player player in players)
+                {
+                    var matchesCount = dp
+                                .Matches
+                                .Find((m => (m.Winner.Id == player.Id || m.Loser.Id == player.Id)))
+                                .ToList().Count;
+
                     result.AppendLine($"Id: {player.Id} | Name: {player.FirstName} {player.LastName} | Matches Played: {matchesCount}");
                 }
             }
