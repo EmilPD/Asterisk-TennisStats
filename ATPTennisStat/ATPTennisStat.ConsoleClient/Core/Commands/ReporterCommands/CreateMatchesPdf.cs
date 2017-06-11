39b97419fd969dac8580ab2b87a41276b6e29832
Emil Dimitrov <emil_132@abv.bg>
2017-06-11 07:27:28 +0300

    Implemented Matches report command

--- ATPTennisStat/ATPTennisStat.ConsoleClient/Core/Commands/ReporterCommands/CreateMatchesPdf.cs
+++ ATPTennisStat/ATPTennisStat.ConsoleClient/Core/Commands/ReporterCommands/CreateMatchesPdf.cs
@@ -1,24 +1,52 @@
-﻿using ATPTennisStat.ConsoleClient.Core.Contracts;
-using ATPTennisStat.SQLServerData;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using ATPTennisStat.ConsoleClient.Core.Contracts;
+using ATPTennisStat.SQLServerData;
+using ATPTennisStat.ReportGenerators;
+using ATPTennisStat.ReportGenerators.Enums;
+using ATPTennisStat.ReportGenerators.Contracts;
 
 namespace ATPTennisStat.ConsoleClient.Core.Commands.ReporterCommands
 {
     class CreateMatchesPdf : ICommand
     {
-        private SqlServerDataProvider dp;
+        private const string Success = "Successfully created matches report";
+        private IReportGenerator reporter;
         private IWriter writer;
+        private ILogger logger;
 
-        public CreateMatchesPdf(SqlServerDataProvider sqlDP, IWriter writer)
+        public CreateMatchesPdf(IReportGenerator reporter, ILogger logger, IWriter writer)
         {
-            this.dp = sqlDP;
+            if (reporter == null)
+            {
+                throw new ArgumentNullException("PdfReportGenerator");
+            }
+
+            if (writer == null)
+            {
+                throw new ArgumentNullException("writer");
+            }
+
+            if (logger == null)
+            {
+                throw new ArgumentNullException("logger");
+            }
+
+            this.reporter = reporter;
             this.writer = writer;
+            this.logger = logger;
         }
 
         public string Execute(IList<string> parameters)
         {
-            throw new NotImplementedException();
+            if (parameters == null)
+            {
+                throw new ArgumentNullException("Execute parameters");
+            }
+
+            this.reporter.GenerateReport(PdfReportType.Matches);
+            this.logger.Log(Success);
+            return Success;
         }
     }
 }
