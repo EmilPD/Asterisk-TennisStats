6757f35a2fe8fc31c8e313ba591b2adaba85ab46
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-06-12 03:38:41 +0300

    Added Mock Classes for City and Country

---
+++ ATPTennisStat/ATPTennisStat.Tests/ConsoleClient/Mocks/CityMock.cs
@@ -0,0 +1,17 @@
+﻿using ATPTennisStat.Models;
+
+namespace ATPTennisStat.Tests.ConsoleClient.Mocks
+{
+    class CityMock : City
+    {
+        public CityMock() : base()
+        {
+        }
+
+        new public int Id { get; set; }
+
+        new public string Name { get; set; }
+
+        new public Country Country { get; set;}
+    }
+}
