819add99d9b61f8dce2870ceb854474dd4f2223f
zachdimitrov <zach_dimitrov@yahoo.com>
2017-06-12 20:59:59 +0300

    Refactored tests, complete BuyTicketCommand_Tests.cs

--- ATPTennisStat/ATPTennisStat.ConsoleClient/Core/Commands/TicketCommands/BuyTicketsCommand.cs
+++ ATPTennisStat/ATPTennisStat.ConsoleClient/Core/Commands/TicketCommands/BuyTicketsCommand.cs
@@ -35,21 +35,28 @@ namespace ATPTennisStat.ConsoleClient.Core.Commands.TicketCommands
             var result = new StringBuilder();
             int ticketId = -1;
 
-            int.TryParse(parameters[0], out ticketId);
-            Guard.WhenArgument(ticketId, "Incorrect event Id!").IsLessThan(0).Throw();
-
-            var currentTickets = dp.Tickets.Find(t => t.Id == ticketId).FirstOrDefault();
-            if (currentTickets != null)
+            if (parameters.Count > 0)
             {
-                currentTickets.Number--;
-                dp.UnitOfWork.Finished();
-                result.AppendLine($"Successfully bought ticket for {currentTickets.TennisEvent.Name}!");
+                int.TryParse(parameters[0], out ticketId);
+                Guard.WhenArgument(ticketId, "Incorrect event Id!").IsLessThan(0).Throw();
+
+                var currentTickets = dp.Tickets.Find(t => t.Id == ticketId).FirstOrDefault();
+                if (currentTickets != null)
+                {
+                    currentTickets.Number--;
+                    dp.UnitOfWork.Finished();
+                    result.AppendLine($"Successfully bought ticket for {currentTickets.TennisEvent.Name}!");
+                }
+                else
+                {
+                    result.AppendLine($"Sorry no tickets with this Id: {ticketId} were found!");
+                }
             }
             else
             {
-                result.AppendLine("Sorry no tickets with this Id were found!");
+                throw new ArgumentOutOfRangeException("Parameter is missing!");
             }
-            
+
             result.AppendLine("[menu] [alle] [allt] [buyt (id)]");
             return result.ToString();
         }
