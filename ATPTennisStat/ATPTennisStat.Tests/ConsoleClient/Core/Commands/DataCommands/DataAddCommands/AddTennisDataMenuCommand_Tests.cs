14fd2bb1f3263ac8ce80986796f075e6766a46b6
zachdimitrov <zach_dimitrov@yahoo.com>
2017-06-12 18:08:59 +0300

    Created AddTennisDataMenuCommand_Tests.cs

---
+++ ATPTennisStat/ATPTennisStat.Tests/ConsoleClient/Core/Commands/DataCommands/DataAddCommands/AddTennisDataMenuCommand_Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Moq;
+using NUnit.Framework;
+using ATPTennisStat.Tests.ConsoleClient.Mocks;
+using ATPTennisStat.Models.SqlServerModels;
+using ATPTennisStat.ConsoleClient.Core.Contracts;
+using ATPTennisStat.ConsoleClient.Core.Commands.DataCommands.DataShowCommands;
+using ATPTennisStat.SQLServerData;
+using ATPTennisStat.ConsoleClient.Core.Commands.MenuCommands;
+
+namespace ATPTennisStat.Tests.ConsoleClient.Core.Commands.DataCommands.DataAddCommands
+{
+    [TestFixture]
+    class AddTennisDataMenuCommand_Tests
+    {
+        [Test]
+        public void ConstructorShould_ReturnInstanceOfAddCountryCommandClass_WhenThePassedValuesAreValid()
+        {
+            var writerMock = new Mock<IWriter>();
+            var command = new AddTennisDataMenuCommand(writerMock.Object);
+
+            Assert.IsInstanceOf<AddTennisDataMenuCommand>(command);
+        }
+
+        [Test]
+        public void ConstructorShould_ThrowArgumentNullException_WhenThePassedWriterIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new AddTennisDataMenuCommand(null));
+        }
+
+        [Test]
+        public void ExecuteShould_ClearTheScreenOneTIme()
+        {
+            var writerMock = new Mock<IWriter>();
+            var command = new AddTennisDataMenuCommand(writerMock.Object);
+
+            command.Execute(new List<string>());
+
+            writerMock.Verify(x => x.Clear(), Times.Once);
+        }
+    }
+}
